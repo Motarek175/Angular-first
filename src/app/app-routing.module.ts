e02ad753347b4814c50ec2ffb493e80e6820433f
mohaed tarek <fcb6480@gmail.com>
2024-10-24 18:20:17 +0200

    Prepare for deployment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { PortfolioComponent } from './Component/portfolio/portfolio.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { AboutComponent } from './Component/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { PortfolioComponent } from './Component/portfolio/portfolio.component';
+import { ContactComponent } from './Component/contact/contact.component';
+import { AboutComponent } from './Component/about/about.component';
+import { NavComponent } from './Component/nav/nav.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PortfolioComponent,
+    ContactComponent,
+    AboutComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
